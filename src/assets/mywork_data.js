70191d392c138ff8d53dda857330f7b5fd4f0ec4
SanthoshKumar211299 <subjansan@gmail.com>
2025-06-27 14:59:39 +0530

    project 2 and github link added

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,5 +1,5 @@
-import project_1 from '../assets/project_1.png'
-import project2_img from '../assets/project_2.svg'
+import project_1_img from '../assets/project_1.png'
+import project2_img from '../assets/GreenCart.png'
 import project3_img from '../assets/project_3.svg'
 import project4_img from '../assets/project_4.svg'
 import project5_img from '../assets/project_5.svg'
@@ -9,15 +9,20 @@ const mywork_data = [
     {
         w_no:1,
         w_name:"Sandy Travel App",
-        w_img:project_1,
+        w_img:project_1_img,
         w_desc:"A travel application for exploring destinations, booking hotels, and creating itineraries.",
         w_desc: "Created a web application to assist users in discovering dream destinations and booking travel tickets. Features include destination details, ticket booking, user reviews, and a responsive UI using Tailwind CSS. Implemented user state management using React Context API.",
-        live_link:"https://sandytravelsfrontend.netlify.app"
+        live_link:"https://sandytravelsfrontend.netlify.app",
+        w_link:'https://github.com/SanthoshKumar211299/Travel_Application_Frontend'
     },
     {
         w_no:2,
-        w_name:"Web design",
-        w_img:project2_img
+        w_name:"Green Cart App",
+        w_img:project2_img,
+        w_desc: "GreenCart is a full-stack e-commerce platform designed for seamless online shopping. It supports user authentication, product browsing, cart and address management, and order placement using Cash on Delivery or Stripe-powered online payments.",
+        live_link:'https://app-green-cart.vercel.app',
+        w_link:'https://github.com/SanthoshKumar211299/AppGreenCart'
+
     },
     {
         w_no:3,
